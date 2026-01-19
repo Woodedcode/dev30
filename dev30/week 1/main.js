92080b9cd27535ba580529bbeb02472b3f635bef
Ryan <Woodedcode@gmail.com>
2026-01-19 14:48:21 -0600

    Add files via upload

---
+++ dev30/week 1/main.js
@@ -0,0 +1,22 @@
+// Own your learning
+//-take it upon myself to learn it and dive deeper
+// Be curious
+// The forecast for today
+const kelvin = 293;
+
+//celsius is kelvin
+const celsius = kelvin - 272;
+
+// formula for celsius to fahrenheit
+//round result down to nearest whole number
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+const newton = Math.floor(celsius * (33/100));
+
+console.log(`The temperatures is ${newton} degrees Newton`)
+
+
+
+
