d05e6135816a26bf939be741dc19791e2e5122d9
Ryan <Woodedcode@gmail.com>
2026-01-19 21:04:02 -0600

    Add files via upload

---
+++ dev30/week 1/data-types.js
@@ -0,0 +1,40 @@
+/*
+PRIMITIVE TYPES
+
+STRING
+-SET OF CHARACTERS in "", '', or ``
+let name = "Ryan"
+
+
+BOOLEAN
+-TRUE OR FALSE - MOST USED IN CONDITIONALS
+
+if (name === 'Ryan){
+} else {
+}
+
+NUMBER
+10,15,4000
+
+NULL
+-NOTHING
+
+UNDEFINED
+- HAS BEEN DECLARED BUT NOT DEFINED
+
+SYMBOL
+
+
+//SINGLE COMPLEX DATA TYPE
+OBJECT
+const car = {
+    color: "Red",
+    speed: "fast";
+    wheels: 4,
+    drive: function () {
+    console.log('vroooom')}
+ }
+
+
+
+*/
