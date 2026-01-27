94d023da504c088f4315125ca373005a6ce4214d
Ryan <Woodedcode@gmail.com>
2026-01-26 21:32:04 -0600

    Add files via upload

---
+++ dev30/week 2/es5-vs-es6.js
@@ -0,0 +1,21 @@
+//ECMAScript
+//DECLARING VARIABLES
+var madeUpName = 'Forky'
+console.log(typeof madeUpName)
+console.log(madeUpName)
+var myAge = 36
+console.log(typeof myAge)
+console.log(myAge)
+var myFamily = ['Annie', 'Olive', 'Johnny', 'June']
+console.log(typeof myFamily)
+console.log(myFamily)
+var myCar = {
+    color: 'Grey',
+    make: 'Toyota',
+    model: '4Runner',
+    year: 2017
+}
+console.log(typeof myCar)
+console.log( myCar)
+
+//JS is loosely typed
