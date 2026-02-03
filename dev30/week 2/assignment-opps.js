8c5d6d6139a208e6ad2e9b80f403aac81d3a2785
Ryan <Woodedcode@gmail.com>
2026-02-02 21:37:33 -0600

    Add files via upload

---
+++ dev30/week 2/assignment-opps.js
@@ -0,0 +1,27 @@
+// Mathematical Assignment Operators
+// +=
+// -+
+// *=
+// /+
+
+
+var myWeight = 180
+
+
+// eat a burger 
+// var burgerWeight = (myWeight + 20)
+// console.log(burgerWeight)
+// myWeight = myWeight + 20
+// console.log(myWeight)
+
+myWeight += 20
+console.log('Eating burgers...' + myWeight)
+// run 3 miles
+myWeight -= 30
+console.log('Running a bunch...' + myWeight)
+// become the hulk
+myWeight *= 4
+console.log('Becoming the Hulk...' + myWeight)
+// become ant man
+myWeight /= 100
+console.log('Becoming the Antman...' + myWeight)
