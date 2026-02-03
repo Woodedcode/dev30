965ee269c753e1262a3c758df4aa4ce6d64c6d97
Ryan <Woodedcode@gmail.com>
2026-02-02 20:39:07 -0600

    Refactor js-engine.js with comments on variable scope
    Added comments explaining variable declaration and execution phases.

--- dev30/week 2/js-engine.js
+++ dev30/week 2/js-engine.js
@@ -1,3 +1,13 @@
-console.log(myName)
-var myName = 'Ryan'
-console.log(myName)
+console.log(myName);
+var myName = 'Ryan';
+console.log(myName);
+
+// 1. Compilation
+// LHS - What are the variables?
+var myName
+
+// 2. Executtion
+// RHS - What are the values of the variables
+console.log(myName) // undefined
+
+myName - 'Ryan'
