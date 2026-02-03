52e056294479e431c011a27ca539d0e28949db5f
Ryan <Woodedcode@gmail.com>
2026-02-02 21:18:02 -0600

    Update doing-math.js

--- dev30/week 2/doing-math.js
+++ dev30/week 2/doing-math.js
@@ -1,26 +1,46 @@
 // addition
-var a = 100 + 70 + 12 +10
-console.log(a)
+// var a = 100 + 70 + 12 +10
+// console.log(a)
 
 // subtraction
-var b = 100-30
-console.log(b)
+// var b = 100-30
+// console.log(b)
 
 
 // multiplication
-var c = 10 * 2
-console.log(c)
+// var c = 10 * 2
+// console.log(c)
 
 
 // division
-var d = 100 / 10
-console.log(d)
+// var d = 100 / 10
+// console.log(d)
 
 
 
 // remainder
-var e = 200 % 300
-console.log(e)
+// var e = 200 % 300
+// console.log(e)
 
+// PART 2
+
+
+var sumOf5and10 = 5 + 10
+console.log(sumOf5and10)
+
+var loss = 50 - 5 
+console.log(loss)
+
+var testAvg = (87 + 94 + 64 + 55 + 100) / 5
+console.log(testAvg)
+
+var test1 = 87
+var test2 = 94
+var test3 = 64
+var test4 = 55
+var test5 = 100
+
+var testAverage = (test1 + test2 + test3 + test4 + test5) / 5
+console.log(testAverage)
 
 
