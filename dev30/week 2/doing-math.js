655a357091a08adcd35609c5b7469be1eedbdff1
Ryan <Woodedcode@gmail.com>
2026-02-02 21:02:15 -0600

    Add files via upload

---
+++ dev30/week 2/doing-math.js
@@ -0,0 +1,26 @@
+// addition
+var a = 100 + 70 + 12 +10
+console.log(a)
+
+// subtraction
+var b = 100-30
+console.log(b)
+
+
+// multiplication
+var c = 10 * 2
+console.log(c)
+
+
+// division
+var d = 100 / 10
+console.log(d)
+
+
+
+// remainder
+var e = 200 % 300
+console.log(e)
+
+
+
