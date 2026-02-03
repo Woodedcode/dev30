9a8a50edc8d3acd776171af34ecb80477244cbdb
Ryan <Woodedcode@gmail.com>
2026-02-02 20:54:55 -0600

    Add files via upload

---
+++ dev30/week 2/misc-basics.js
@@ -0,0 +1,17 @@
+var myName = "ryan"
+console.log(myName)
+
+// COMMENTS
+//this is my name - single line
+/*
+
+multi line
+
+*/
+
+
+var myName = "ryan"
+
+// var myName = 'annie'
+
+console.log(myName)
