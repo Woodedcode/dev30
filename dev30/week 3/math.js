be1070e8d09466c40e6b9aaa2906046288c4be65
Ryan <Woodedcode@gmail.com>
2026-02-06 09:44:01 -0600

    Add files via upload

---
+++ dev30/week 3/math.js
@@ -0,0 +1,23 @@
+// min
+var num1 = 342;
+var num2 = 3453;
+
+console.log(("Min",Math.min(num1, num2)); // 342
+
+// max
+var num1 = 342;
+var num2 = 3453;
+
+console.log("Max",Math.max(num1, num2)); // 3453
+
+// floor
+console.log("Floor",Math.floor(7.56)); // 7
+
+// ceil
+console.log("Ceil",Math.ceil(7.56)); // 8
+
+// round
+console.log("round",Math.round(3.44)); // 3
+
+// random
+console.log("random",Math.floor(Math.random() * 100)); // Random number between 0 and 100
